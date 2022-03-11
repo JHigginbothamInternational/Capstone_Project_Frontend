9365d5983c4101d5fb078814c89292b2e65d95fd
jacobhigginbotham <j.higginbotham.international@gmail.com>
2022-03-10 22:58:18 -0600

    added image for smoke element

---
+++ src/components/smoke/smoke.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import SmokeElement from "smoke-effect-react";
+
+export default class Smoke extends Component {
+    constructor() {
+        super()
+
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div className="">
+                
+            </div>
+        )
+    }
+}
