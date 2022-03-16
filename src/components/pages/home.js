8d9c40d896d701cad1588a6c4ebceb8bb54c6186
jacobhigginbotham <j.higginbotham.international@gmail.com>
2022-03-15 23:05:41 -0500

    using react-magic

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,9 +1,23 @@
-import React from "react"
+import React, { Component, PropTypes } from "react";
+import { StyleSheet, css } from "aphrodite";
+
+import twisterInDown from "react-magic";
+
+const styles = StyleSheet.create({
+    magic: {
+        swap: twisterInDown,
+        animationDuration: "2s",
+    },
+});
 
 export default function Home(props) {
     return (
-        <div className="content-wrapper">         
-            <h1>Oh hi, Mark.</h1>
+        <div className="content-wrapper-home">         
+            <div className="empty-space"></div>
+            <div className={css(styles.magic)}>
+                <h3>Welcome</h3>
+            </div>
+            <div className="empty-space"></div>
         </div>
    )
 }
