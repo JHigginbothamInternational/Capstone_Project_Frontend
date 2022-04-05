41145b63e728450c61d52aa65069b40606103232
jacobhigginbotham <j.higginbotham.international@gmail.com>
2022-04-04 22:40:06 -0500

    Finished about page & styles for home page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -3,7 +3,13 @@ import React from "react"
 export default function About (props) {
     return (
         <div className="content-wrapper">          
-            <h1>This page is about this app.</h1>
+            <h1>Welcome to the Text-Adventure app by Jacob Higginbotham.<br /><br />
+            In this app, you will navigate around the webpage, sign-up, login, logout, <br /><br />
+            and try this game out.  I hope you enjoy and this will be developed overtime.<br /><br />
+            <br /><br /><br /><br />
+            Check for developer notes as the page develops.<br /><br /><br /><br />
+            Thanks for playing!
+            </h1>
         </div>
    )
 }
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -4,12 +4,15 @@ import 'animate.css';
 export default function Home(props) {
     return (
         <div className="content-wrapper-home">
+            <div className="text-wrapper">
                 <div className="text">
                     <span className="welcome">W</span><span className="welcome">e</span><span className="welcome">l</span><span className="welcome">c</span><span className="welcome">o</span><span className="welcome">m</span><span className="welcome">e</span>
                 </div>
                 <div className="other-text">
                     <h5 className="animate__animated animate__fadeIn animate__delay-4s">Please sign in to play.</h5>
                 </div>
+            </div>
+            <div className="home-spacer"></div>               
         </div>
    )
 }
